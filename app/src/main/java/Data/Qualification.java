7d90f7ef837717794d4e30601494346392712887
scm2217 <smendonc@ucsc.edu>
2015-01-10 03:42:19 -0800

    qualificaitons

---
+++ app/src/main/java/Data/Qualification.java
@@ -0,0 +1,13 @@
+/**
+ * Created by HP on 1/10/2015.
+ */
+public class Qualification {
+    //This is a very simple object which contains a tutor's qualification level in a certain class
+    public String subject;
+    public double skill; //0 = incompetant, 1 = proficient, 2 = advanced, 3 = expert
+
+    public Qualification(String sub, double sk){
+        subject = sub;
+        skill = sk;
+    }
+}
--- app/src/main/java/Data/Tutor.java
+++ app/src/main/java/Data/Tutor.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
  * Created by HP on 1/10/2015.
  */
 public class Tutor extends Member{
-    ArrayList<String> qualified = new ArrayList<>(0); //will contain classes qualified to teach in
     private double hourlyRate; //cost per hour
     private double rawScore = 0; //the sum of all reviews for this tutor
     private double numberRatings = 0; //this is the number reviews this instructor has received
     private double averageRating;
+    ArrayList<Qualification> qualifications = new ArrayList<>(0);
 
     //constructor
     public Tutor(String na, String em, String pass, String ph){
@@ -17,14 +17,6 @@ public class Tutor extends Member{
         super(na, em, pass, sk, ph);
     }
 
-    public void addClass(String newClass) {
-        if(!qualified.contains(newClass)) //make sure we don't clone classes
-            qualified.add(newClass);
-    }
-    public void removeClass(String dumpClass){
-        if(qualified.contains(dumpClass))
-            qualified.remove(dumpClass);
-    }
     public void setPrice(double price){
         hourlyRate = price;
     }
@@ -47,5 +39,22 @@ public class Tutor extends Member{
     }
     public double get_numOfRatings(){
         return numberRatings;
+    }
+    public void changeQualification(String subject, double level){
+        for(int i = 0; i<qualifications.size(); i++){
+            if(qualifications.get(i).subject.equals(subject)){
+                Qualification updated = qualifications.get(i);
+                updated.skll = level;
+                qualifications.set(i, updated);
+                return; //exit the method once we have updated the level of qualification
+            }
+        }
+        //if class wasnt found then we add it
+        qualfications.add(new Qualification(subject, level))
+    }
+    public void removeQualification(Qualification toRemove){
+        if(qualifications.contains(toRemove)){
+            qualifications.remove(toRemove);
+        }
     }
 }
