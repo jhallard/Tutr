93c237e5e73c106ffb4e46a41bbd40453ecfd747
scm2217 <smendonc@ucsc.edu>
2015-01-10 21:46:30 -0800

    front end tempalte

---
+++ app/src/main/java/com/ucsc/teamtutor/tutr/Model/FrontEndNode.java
@@ -0,0 +1,30 @@
+package com.ucsc.teamtutor.tutr.Model;
+
+/**
+ * Created by HP on 1/10/2015.
+ */
+public class FrontEndNode {
+    static LogInVerification verifier;
+    static boolean updateStudent(Student updated){
+        boolean success = false;
+        return success;
+    }
+    static boolean createStudent(Student updated){
+        boolean success = false;
+        return success;
+    }
+    static boolean deleteStudent(Student updated){
+        boolean success = false;
+        return success;
+    }
+    static Tutor getNearestTutors(String subject, double longitude, double latitude){
+        //TODO find nearby tutors using gps finder
+        Tutor nearByTutors = null;
+        return nearByTutors;
+    }
+    static int verifyUserInfo(String username, String password){
+        int validInfo = 2; //0 = success, 1 = password fail, 2 = username fail
+        //TODO pull data from database and compare to data
+        return validInfo;
+    }
+}
--- app/src/main/java/com/ucsc/teamtutor/tutr/Model/main.java
+++
@@ -1,9 +0,0 @@
-package com.ucsc.teamtutor.tutr.Model;
-
-/**
- * Created by HP on 1/10/2015.
- */
-public class main {
-    public static void main(String[] args){
-    }
-}
