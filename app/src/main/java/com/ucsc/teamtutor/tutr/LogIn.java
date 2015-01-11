7eb6e9a73ea357490987be8c941abcde5f18e480
scm2217 <smendonc@ucsc.edu>
2015-01-10 17:30:12 -0800

    login incomplete

--- app/src/main/java/com/ucsc/teamtutor/tutr/LogIn.java
+++ app/src/main/java/com/ucsc/teamtutor/tutr/LogIn.java
@@ -1,5 +1,7 @@
 package com.ucsc.teamtutor.tutr;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -12,7 +14,7 @@ import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.EditText;
 
 public class LogIn extends ActionBarActivity {
 
@@ -20,6 +22,37 @@ public class LogIn extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button login = (Button) findViewById(R.id.ATTEMPT_LOGIN);
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                //Pass in login data to check
+                EditText emailField = (EditText) findViewById(R.id.EMAIL);
+                EditText passwordField = (EditText) findViewById(R.id.PASSWORD);
+                String Email = emailField.getText().toString();
+                String Password = passwordField.getText().toString();
+            }
+        });
+
+        Button signUp = (Button) findViewById(R.id.SIGNUP);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //bring up sign-up fragment
+                Fragment signUp = new PlaceholderFragment();
+
+                FragmentManager fm  = getFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                transaction.commit();
+            }
+        });
+
+        Button forgot = (Button) findViewById(R.id.FORGOT_PASSWORD);
+        forgot.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TBD
+            }
+        });
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -75,7 +108,7 @@ public class LogIn extends ActionBarActivity {
         public void onClick(View v) {
             switch(v.getId()){
                 case R.id.SIGNUP:
-                    ((TextView)rootView.findViewById(R.id.SIGNUP)).setText("Completing Sign Up");
+                    ((TextView)rootView.findViewById(R.id.SIGNUP)).setText("Signing Up");
                     break;
 
             }
